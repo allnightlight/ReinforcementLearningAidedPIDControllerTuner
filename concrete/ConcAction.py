6cbfa89159fa407d8834442dd00ae947405a6d38
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-15 19:04:31 +0900

    Implemented the feature of continuous action as ConcAction
    Implemented the following feature of continuous action as ConcAction:
    * ConcAction reserves the output of an agent
    * ConcAction converts and ceils the raw output so that environments can
      receive.

--- concrete/ConcAction.py
+++ concrete/ConcAction.py
@@ -4,33 +4,29 @@ Created on 2020/05/03
 @author: ukai
 '''
 
-import numpy as np
 from framework import Action
+import numpy as np
+
 
 class ConcAction(Action):
     '''
     classdocs
     '''
 
-    def __init__(self, _pLever):
+    def __init__(self, _u):
         '''
         Constructor
         '''
-        assert len(_pLever.shape) == 2 
-        self._pLever = _pLever # (*, nLevers,)
+        assert len(_u.shape) == 2 
+        self._u = _u # (*, nMv,)
         
-        nBatch, nLevers = _pLever.shape
-        
-        threshold = np.cumsum(_pLever.numpy(), axis=-1) # (*, nLevers,)
-        iSelected = np.sum(np.random.rand(nBatch, 1) < threshold, axis=-1) - 1 # (*,)
-        assert np.all((iSelected >= 0) & (iSelected < nLevers)) 
-        
-        self.selectedAction = np.zeros((nBatch, nLevers,)).astype(np.float32) # (*, nLevers,)
-        for i, j in enumerate(iSelected):
-            self.selectedAction[i, j] = 1
+        # _u is not used as a manipulated value for the environment,
+        # instead, what should be used is the value ceiling u   
+        # after having converted as numpy.ndarray 
+        self.actionOnEnvironment = np.tanh(_u.numpy()).astype(np.float32) # (*, nMv,)
         
     def getValue(self):
-        return self._pLever # (*, nLevers,)
+        return self._u # (*, nMv,)
     
-    def getSelectedAction(self):
-        return self.selectedAction # (*, nLevers,)
+    def getActionOnEnvironment(self):
+        return self.actionOnEnvironment # (*, nMv,)
