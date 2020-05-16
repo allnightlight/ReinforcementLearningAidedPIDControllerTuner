0add243324a80008a8a8c6d5953bae673237989a
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-16 17:45:25 +0900

    Removed the java source code from the released source codes
    Removed the java source codes which were used to design the framework of
    RL training, which have been alredy meaningless.

--- framework/java/src/framework/ObservationSequence.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-public class ObservationSequence extends MyArray<Observation>{
-	
-	public ObservationSequence copy() {
-		return (ObservationSequence)this.clone();
-	}
-
-}
--- framework/java/src/framework/RewardGiver.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-public class RewardGiver {
-	
-	public Reward evaluate(ObservationSequence observationSequence, Action action) {
-		return new Reward();
-	}
-
-}
