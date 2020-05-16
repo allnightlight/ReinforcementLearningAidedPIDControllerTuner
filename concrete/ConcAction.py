7ab1ee7f3f2521bc023e5214d7c4ba5cc8f5e0e7
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-16 17:05:15 +0900

    Modified the construct parameter type from  tensor to np.ndarray
    Modified the construct parameter type from  tensor to np.ndarray

--- concrete/ConcAction.py
+++ concrete/ConcAction.py
@@ -13,20 +13,21 @@ class ConcAction(Action):
     classdocs
     '''
 
-    def __init__(self, _u):
+    def __init__(self, u):
         '''
         Constructor
         '''
-        assert len(_u.shape) == 2 
-        self._u = _u # (*, nMv,)
+        assert len(u.shape) == 2
+        assert type(u) is np.ndarray 
+        self.u = u.astype(np.float32) # (*, nMv,)
         
-        # _u is not used as a manipulated value for the environment,
+        # u is not used as a manipulated value for the environment,
         # instead, what should be used is the value ceiling u   
         # after having converted as numpy.ndarray 
-        self.actionOnEnvironment = np.tanh(_u.numpy()).astype(np.float32) # (*, nMv,)
+        self.actionOnEnvironment = np.tanh(u).astype(np.float32) # (*, nMv,)
         
     def getValue(self):
-        return self._u # (*, nMv,)
+        return self.u # (*, nMv,)
     
     def getActionOnEnvironment(self):
         return self.actionOnEnvironment # (*, nMv,)
