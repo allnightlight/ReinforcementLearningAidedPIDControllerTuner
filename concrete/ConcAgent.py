7ab7a574d4d4bc8a5724e9262edc0de1a9f1477e
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-16 17:00:28 +0900

    Implemented the feature of agent to return loglikelihood
    The policy of the agent represents a conditional probabilistic function
    given a state, which is, in this case, he normal distribution.
    Implemented the feature to return the loglikelihood, which is required
    by policy optimizer.

--- concrete/ConcAgent.py
+++ concrete/ConcAgent.py
@@ -10,6 +10,7 @@ from ConcAgentMemento import ConcAgentMemento
 from Utils import Utils
 from framework import Agent, ObservationSequence
 import tensorflow as tf
+import numpy as np
 
 
 class ConcAgent(Agent, tf.keras.Model):
@@ -20,7 +21,7 @@ class ConcAgent(Agent, tf.keras.Model):
     checkpointFolderPath = "./checkpoint"
 
 
-    def __init__(self, nMv):
+    def __init__(self, nMv, sd):
         '''
         Constructor
         '''
@@ -28,16 +29,47 @@ class ConcAgent(Agent, tf.keras.Model):
         
         self.gainP = tf.keras.layers.Dense(nMv) # (*, nPv) -> (*, nMv)
         
-    def call(self, observationSequence):
-        # input: an instance of observationSequence is not used here.
+# This agent has the fixed standard deviation, sd, of the normal density function which represents the policy.
+# It means that the policy is represented as like this:
+# pi(a|s) = Normal(a|mu, sd), 
+# where mu = a neural network(s) 
+# and sd is the parameter given at the initialization.
+
+        self.sd = sd # (1, nMv)
+        
+    # <<private>>
+    def getProbabilisticFunctionParameter(self, observationSequence):
         
         assert isinstance(observationSequence, ObservationSequence)
         obserbation = observationSequence.get(-1) # the latest observation
         y = obserbation.getValue() # (*, nPv)
         
-        _u = self.gainP(y) # (*, nMv)
+        _mu = self.gainP(y) # (*, nMv)
+        sd = self.sd # (1, nMv)
+        return _mu, sd
         
-        return ConcAction(_u)
+    def call(self, observationSequence):
+        # input: an instance of observationSequence is not used here.
+        
+        _mu, sd = self.getProbabilisticFunctionParameter(observationSequence)
+        # _mu: (*, nMv), sd: (*, nMv)
+        mu = _mu.numpy() # (*, nMv)
+
+        u = mu + np.random.randn(mu.shape[0], 1) * sd # (*, nMv) 
+        
+        return ConcAction(u)
+    
+    def loglikelihood(self, observationSequence, action):
+        
+        _mu, sd = self.getProbabilisticFunctionParameter(observationSequence)
+        # _mu: (*, nMv), sd: (*, nMv)
+        
+        u = action.getValue() # (*, nMv)
+
+        _ll = tf.reduce_sum(-tf.math.log(sd) - (u - _mu)**2/(sd**2)/2, axis=-1, keepdims=True) # (*, 1)
+        
+        return _ll #(*, 1)
+        
     
     def createMemento(self):
         if not os.path.exists(ConcAgent.checkpointFolderPath):
