56f9f07336f66b07ac0b6b0b4fdca5f8938418fc
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-16 17:45:25 +0900

    Removed the java source code from the released source codes
    Removed the java source codes which were used to design the framework of
    RL training, which have been alredy meaningless.

--- framework/java/src/framework/MyLogger.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-public class MyLogger {
-	
-	public void info(TrainId trainId, StoreField storeField) {
-	}
-
-
-}
--- framework/java/src/framework/ValueFunctionApproximator.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-public class ValueFunctionApproximator {
-	
-	public Value call(ObservationSequence observationSequence) {
-		return new Value();
-	}
-
-}
