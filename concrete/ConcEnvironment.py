9c3cca8f5f0fa2b31e5fbfebd4c0cae997747317
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-18 18:57:58 +0900

    Implemented the feautre of periodic disturbance  on the environment
    The target environment will be the periodic disturbance in addition to
    the dv following the white noise and the Mv.

--- concrete/ConcEnvironment.py
+++ concrete/ConcEnvironment.py
@@ -15,12 +15,14 @@ class ConcEnvironment(Environment):
     nPv = 1
     nMv = 1
 
-    def __init__(self, tConstant, amplitudeDv = 0.1):
+    def __init__(self, tConstant, amplitudeDv = 0.1, amplitudePeriodicDv = 0.0, cyclePeriodicDv = 20):
         '''
         Constructor
         '''
         
         self.x = None
+        self.t = None
+        self.dvPeriodic = None
         self.alpha = 1 - 1/tConstant # e.g. 0.99 = 1 - 1/100 with tConstant = 100
         self.beta = 1/np.sqrt(1 - self.alpha**2)
         # to normalize the amplitude of disturbance 
@@ -28,8 +30,13 @@ class ConcEnvironment(Environment):
         self.gamma = amplitudeDv
         # the influence of disturbance on x is controlled by gamma
         
+        self.amplitudePeriodicDv = amplitudePeriodicDv
+        self.cyclePeriodicDv = cyclePeriodicDv
+        
     def init(self):
+        self.t = 0
         self.x = np.zeros((1,1)) # (1, nState = 1)
+        self.dvPeriodic = 1.0
         
     def observe(self):
         y = self.x # (1, nState = nPv = 1)
@@ -40,5 +47,12 @@ class ConcEnvironment(Environment):
         
         u = action.getActionOnEnvironment() # (1, nMv)
         w = np.random.randn(1, 1) # (1, nDv = 1)
-        self.x = self.alpha * self.x + (1-self.alpha) * u[:,0,None] + self.gamma * self.beta * w # (1, nState = 1)
+        
+        self.x = self.alpha * self.x \
+            + (1-self.alpha) * (u[:,0,None] + self.amplitudePeriodicDv * self.dvPeriodic) \
+            + self.gamma * self.beta * w # (1, nState = 1)
+            
+        if self.cyclePeriodicDv == self.cyclePeriodicDv - 1:
+            self.dvPeriodic = -1.0 * self.dvPeriodic
+        self.t = (self.t + 1) % self.cyclePeriodicDv
         
