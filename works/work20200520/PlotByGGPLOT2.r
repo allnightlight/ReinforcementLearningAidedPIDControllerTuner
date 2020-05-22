0a0af24218e0fa5f021a07a1aef20fa78ca615ee
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-22 17:02:41 +0900

    Added the feature to plot the proportional gain of P-controller

--- works/work20200520/PlotByGGPLOT2.r
+++ works/work20200520/PlotByGGPLOT2.r
@@ -1,13 +1,26 @@
 
 library(ggplot2)
 
-df = read.csv("data.csv")
+
+
+
+df = read.csv("./tmp/data.csv")
 
 p = ggplot(df, aes(x=time, y=value, color = varType))
 p = p + geom_line()
-p = p + facet_grid(weightOnError~epoch)
+p = p + facet_grid(weightOnError ~ epoch) 
 p = p + xlab('Simulation steps')
 p = p + ylab('')
-ggsave('response.png')
+ggsave('./tmp/response.png')
 print(p)
 
+
+df = read.csv("./tmp/gain.csv")
+
+p = ggplot(df, aes(x = as.factor(epoch), y=value))
+p = p + geom_boxplot()
+p = p + facet_wrap(weightOnError ~ .)
+p = p + xlab('Training iteration')
+p = p + ylab('Gain')
+ggsave('./tmp/gain.png')
+print(p)
