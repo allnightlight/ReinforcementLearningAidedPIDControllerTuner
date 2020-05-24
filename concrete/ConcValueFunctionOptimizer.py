64ebcb27e2069c827687b3f1fb8fa562a948d097
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-24 17:59:23 +0900

    Enabled to represent return by weighted sum of infinite reward series
    The return given the agent can be described in the following ways:
    * the finite sum of rewards,
    * and the infinite weighted-sum of rewards.
    The parameter of build order: "returnType" enables to control the type.

--- concrete/ConcValueFunctionOptimizer.py
+++ concrete/ConcValueFunctionOptimizer.py
@@ -15,7 +15,7 @@ class ConcValueFunctionOptimizer(ValueFunctionOptimizer):
     '''
 
 
-    def __init__(self, valueFunctionApproximator, agent, nHorizonValueOptimization, optimizer = "Adam", learningRate=1e-3):
+    def __init__(self, valueFunctionApproximator, agent, nHorizonValueOptimization, optimizer = "Adam", learningRate=1e-3, returnType = "SumOfFiniteRewardSeries", gamma = None):
         '''
         Constructor
         '''
@@ -26,12 +26,13 @@ class ConcValueFunctionOptimizer(ValueFunctionOptimizer):
         if optimizer == "RMSprop":
             self.optimizer = tf.keras.optimizers.RMSprop(learning_rate = learningRate)            
         self.countUpdate = 0
-        
-        
-        
-    def train(self, observationSequences, actions, rewards):
-        self.countUpdate += 1
-        
+        self.returnType = returnType
+        if returnType == "SumOfInfiniteRewardSeries":
+            assert gamma is not None
+        self.gamma = gamma
+
+    def trainWithSumOfFiniteRewardSeries(self, observationSequences, actions, rewards):
+
         Nstep = len(actions)
         assert len(observationSequences) == Nstep + 1
         assert len(rewards) == Nstep
@@ -57,3 +58,51 @@ class ConcValueFunctionOptimizer(ValueFunctionOptimizer):
         _Grad = gtape.gradient(_Loss, self.valueFunctionApproximator.trainable_variables)
         
         self.optimizer.apply_gradients(zip(_Grad, self.valueFunctionApproximator.trainable_variables))
+
+    def trainWithSumOfInfiniteRewardSeries(self, observationSequences, actions, rewards):
+        
+        Nstep = len(actions)
+        assert len(observationSequences) == Nstep + 1
+        assert len(rewards) == Nstep
+        
+        action0 = actions[0]
+        assert isinstance(action0, ConcAction)
+        
+        observationSequence0 = observationSequences[0]
+        observationSequence1 = observationSequences[-1]        
+        assert isinstance(observationSequence0, ObservationSequence)
+        assert isinstance(observationSequence1, ObservationSequence)
+               
+        gammaExponents = [ self.gamma**k1 for k1 in range(self.nHorizonValueOptimization+1)] # (nHor+1,)
+        # gammaExponents = (1, gamma, gamma^2, ..., gamma^(nHor-1), gamma^nHor), where gamma = 1 - 1/nHor.
+        
+        rewardMean = (1-self.gamma) * np.sum([reward.getValue() * gammaExponent for reward, gammaExponent in zip(rewards, gammaExponents) ], axis=0) # (*,)
+        # rewardMean = (1-gamma) * (reward(0) + gamma * reward(1) + ... + gamma^(nHor-1) * reward(nHor-1))
+        
+        action1 = self.agent(observationSequence1)
+             
+        with tf.GradientTape() as gtape:
+                        
+            qValue0 = self.valueFunctionApproximator(observationSequence0, action0) # (*, 1)
+            qValue1 = self.valueFunctionApproximator(observationSequence1, action1) # (*, 1)
+            
+            _q0, _sValue0 = qValue0.getValue() # _q0 = (*, 1), _sValue0 = (*,1)
+            _q1, _sValue1 = qValue1.getValue() # _q1 = (*, 1), _sValue1 = (*,1)
+        
+            _ErrorBellman = _q0 - (rewardMean + gammaExponents[-1] * _q1)# (*,)
+            _ErrorBellmanForStateValueFunction = _sValue0 - (rewardMean + gammaExponents[-1] * _sValue1) # (*,)
+                    
+            _Loss = tf.reduce_mean(_ErrorBellman**2) + tf.reduce_mean(_ErrorBellmanForStateValueFunction**2) #(,)
+        
+        _Grad = gtape.gradient(_Loss, self.valueFunctionApproximator.trainable_variables)
+        
+        self.optimizer.apply_gradients(zip(_Grad, self.valueFunctionApproximator.trainable_variables))
+        
+    def train(self, observationSequences, actions, rewards):
+        
+        self.countUpdate += 1
+        
+        if self.returnType == "SumOfFiniteRewardSeries":
+            self.trainWithSumOfFiniteRewardSeries(observationSequences, actions, rewards)
+        if self.returnType == "SumOfInfiniteRewardSeries":
+            self.trainWithSumOfInfiniteRewardSeries(observationSequences, actions, rewards)
