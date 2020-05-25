ec0e0a67e354c6478c893fcbef2206e301b3e7d7
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-25 21:29:57 +0900

    Implemented an AsmSimulator
    AsmSimulator models a waste water process with an aerobic tank and with
    a daily fluctuating inflow disturbance.

---
+++ concrete/AsmAction.py
@@ -0,0 +1,33 @@
+'''
+Created on 2020/05/25
+
+@author: ukai
+'''
+
+import numpy as np
+
+class AsmAction(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, u, DoMax = 3, DoMin = 0):
+        '''
+        Constructor
+        '''
+        assert len(u.shape) == 2
+        assert type(u) is np.ndarray 
+        self.u = u.astype(np.float32) # (*, nMv,)
+        
+        # u is not used as a manipulated value for the environment,
+        # instead, what should be used is the value ceiling u   
+        # after having converted as numpy.ndarray 
+        self.actionOnEnvironment = DoMin + (DoMax - DoMin) * 1./(1.+np.exp(-u)).astype(np.float32) # (*, nMv,)
+        
+    def getValue(self):
+        return self.u # (*, nMv,)
+    
+    def getActionOnEnvironment(self):
+        return self.actionOnEnvironment # (*, nMv,)
+
+    
