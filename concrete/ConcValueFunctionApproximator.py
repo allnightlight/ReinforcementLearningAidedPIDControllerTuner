0744ee7a1a810c35bea65cc9ec35a8120c39898d
Shouta Ukai <shouta.ukai@gmail.com>
2020-06-21 19:42:37 +0900

    Turned the ConcValueFunctionApproximator to the function of P/I/D components
    Enabled the input of value function approximator to take not only P
    but also I and D components

--- concrete/ConcValueFunctionApproximator.py
+++ concrete/ConcValueFunctionApproximator.py
@@ -14,7 +14,7 @@ class ConcValueFunctionApproximator(ValueFunctionApproximator, tf.keras.Model):
     classdocs
     '''
 
-    def __init__(self, nHiddenValueApproximator):
+    def __init__(self, nHiddenValueApproximator, enable_i_component = False, enable_d_component = False):
         '''
         Constructor
         '''
@@ -26,16 +26,41 @@ class ConcValueFunctionApproximator(ValueFunctionApproximator, tf.keras.Model):
         fnn.add(tf.keras.layers.Dense(1))
         
         self.fnn = fnn # (*, nPv + nMv) -> (*, 1)
+        self.enable_i_component = enable_i_component        
+        self.enable_d_component = enable_d_component
+
 
     def call(self, observationSequence, action):
         observation = observationSequence[-1] # as ConcObservation
-        
         y = observation.getValue() # (*, nPv)
+        
+        Y = []
+        for obserbation in observationSequence:
+            Y.append(obserbation.getValue())
+        Ynumpy = np.stack(Y, axis=-1) # (*, nPv, nSeq)
+        nSeq = len(Y)
+        
+        if not self.enable_i_component:
+            y_i = np.zeros((y.shape[0], 0)).astype(np.float32) # (*, 0)
+        elif nSeq > 1:
+            y_i = np.sum(Ynumpy, axis=-1) # (*, nPv)
+        else:
+            y_i = np.zeros(y.shape).astype(np.float32) # (*, nPv)
+            
+        if not self.enable_d_component:            
+            y_d = np.zeros((y.shape[0], 0)).astype(np.float32) # (*, 0)
+        elif nSeq > 1:
+            t = np.linspace(-(nSeq-1)/2, (nSeq-1)/2, nSeq).astype(np.float32)
+            y_d = np.sum(Ynumpy * t, axis=-1)/np.sum(t*t) # (*, nPv)            
+        else:
+            y_d = np.zeros(y.shape).astype(np.float32) # (*, nPv)
+                    
+        
         u = action.getValue() # (*, nMv)
         uNull = tf.zeros(shape=u.shape) # (*, nMv)
         
-        yu = np.concatenate((y,u), axis=-1) # (*, nPv + nMv)
-        yuNull = np.concatenate((y,uNull), axis=-1) # (*, nPv + nMv)
+        yu = np.concatenate((y,y_i,y_d,u), axis=-1) # (*, nPv + nMv)
+        yuNull = np.concatenate((y,y_i,y_d,uNull), axis=-1) # (*, nPv + nMv)
         
         _aValue = self.fnn(yu) # (*, 1)
         _sValue = self.fnn(yuNull) # (*, 1)
